8dc74a110e8f4b647c44e3c6c023135d447adc50
isabolivarm <isabella.bolivar@correo.unimet.edu.ve>
2024-08-05 14:20:36 -0400

    Revert "Isabella"

--- API.py
+++ API.py
@@ -1,6 +1,7 @@
 import requests as rq
 
-class API: 
+class API:
+
     def cargar_api(self, link):
         response = rq.get(link)
         print(response.json())
@@ -9,24 +10,4 @@ class API:
     def cargar_personajes(self):
         personajes = self.cargar_api('https://www.swapi.tech/api/people')
         return personajes
-
-    def cargar_films(self):
-        db_films = self.cargar_api("https://www.swapi.tech/api/films/")
-        return db_films
-    
-    def cargar_planets(self):
-        db_planets =self.cargar_api("https://www.swapi.tech/api/planets/")
-        return db_planets
-    
-    def cargar_species(self):
-        db_species=self.cargar_api("https://www.swapi.tech/api/species/")
-        return db_species
-    
-    def cargar_starships(self):
-        db_starships=self.cargar_api("https://www.swapi.tech/api/starships/")
-        return db_starships
-    
-    def cargar_vehicles(self):
-        db_vehicles=self.cargar_api("https://www.swapi.tech/api/vehicles/")
-        return db_vehicles
 
--- App.py
+++ App.py
@@ -1,22 +1,9 @@
-
 from API import API
-from Film import Film
-
 
 class App:
-    film_obj=[]
 
     def start(self):
         api = API()
         api.cargar_personajes()
         
         print("Starting the app...")
-
-
-    def crear_films(self):
-        db_films = self.cargar_api("https://www.swapi.tech/api/films/")
-        for film in db_films:
-                self.film_obj.append(Film(film["title"]),(film["episode_id"]),(film["release_date"]),(film["opening_crawl"]),(film["director"]) )
-
-    def mostrar_films(self):
-         
