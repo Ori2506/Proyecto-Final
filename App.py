3458b44fd851057b04628a9bb9ca0e4f6ca064db
isabella <isabella.bolivar@correo.unimet.edu.ve>
2024-08-05 17:39:46 -0400

    creando mision

--- App.py
+++ App.py
@@ -43,27 +43,27 @@ class App:
         while n < 7:
             arma_seleccionada = input("Ingrese una arma o 0 para finalizar: ")
 
-        if arma_seleccionada==0:
-            break
-        elif arma_seleccionada in #lista de armas
-            armas_utilizar.append(arma_seleccionada)
-            n += 1
-        else:
-            print("Arma inválida. Intente nuevamente.")
+            if arma_seleccionada==0:
+                break
+            elif arma_seleccionada in #lista de armas
+                armas_utilizar.append(arma_seleccionada)
+                n += 1
+            else:
+                print("Arma inválida. Intente nuevamente.")
 
-        integrantes_mision=[]
-        m=0
+            integrantes_mision=[]
+            m=0
 
         while n<7:
             integrante_seleccionado= input("Ingrese un integrante de la misión o 0 para finalizar: ")
 
-        if integrante_seleccionado==0:
-            break
-        elif integrante_seleccionado in #lista de people
-            integrantes_mision.append(integrante_seleccionado)
-            m+=1
-        else:
-            print("Integrante inválido. Intente nuevamente")
+            if integrante_seleccionado==0:
+                break
+            elif integrante_seleccionado in #lista de people
+                integrantes_mision.append(integrante_seleccionado)
+                m+=1
+            else:
+                print("Integrante inválido. Intente nuevamente")
 
         
 
