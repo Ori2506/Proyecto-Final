a14748a25aaf5b780f570d416b7895a49a54acd0
isabella <isabella.bolivar@correo.unimet.edu.ve>
2024-08-05 15:34:53 -0400

    montar apis en develop

--- API.py
+++ API.py
@@ -11,3 +11,22 @@ class API:
         personajes = self.cargar_api('https://www.swapi.tech/api/people')
         return personajes
 
+    def cargar_films(self):
+        db_films = self.cargar_api("https://www.swapi.tech/api/films/")
+        return db_films
+    
+    def cargar_planets(self):
+        db_planets =self.cargar_api("https://www.swapi.tech/api/planets/")
+        return db_planets
+    
+    def cargar_species(self):
+        db_species=self.cargar_api("https://www.swapi.tech/api/species/")
+        return db_species
+    
+    def cargar_starships(self):
+        db_starships=self.cargar_api("https://www.swapi.tech/api/starships/")
+        return db_starships
+    
+    def cargar_vehicles(self):
+        db_vehicles=self.cargar_api("https://www.swapi.tech/api/vehicles/")
+        return db_vehicles
